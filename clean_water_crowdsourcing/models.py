f0fef2e4a1a239bf9924e62187044e9738040a7a
Sruti Guhathakurta <sruti@gatech.edu>
2016-11-23 15:15:46 -0500

    started on models.py

--- clean_water_crowdsourcing/models.py
+++ clean_water_crowdsourcing/models.py
@@ -2,4 +2,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    type_choices = (("U", "User"), ("W", "Worker"), ("M", "Manager"), ("A", "Admin"))
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    username = models.CharField(max_length=20, primary_key = True)
+    address_street = models.CharField(max_length=20)
+    address_city = models.CharField(max_length=20)
+    address_state = models.CharField(max_length=2)
+    address_zip = models.CharField(max_length=10)
+    account_type = models.CharField(choices=type_choices, max_length=1)
+
+    def __str__(self):
+        return self.first_name + self.last_name
