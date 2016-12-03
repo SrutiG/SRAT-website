23925d25610b1f6be80d0e49c18107f4f74b5edb
Sruti Guhathakurta <sruti@gatech.edu>
2016-12-03 00:52:34 -0500

    added javascript for submit source report

--- clean_water_crowdsourcing/static/clean_water_crowdsourcing/js/index.js
+++ clean_water_crowdsourcing/static/clean_water_crowdsourcing/js/index.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
         parent.history.back();
         return false;
     });
+    $("#submit-source-nav").click(function() {
+        $(".submit-source-report").show();
+        $(".main").css("opacity", 0.3);
+    })
+    $("#cancel-source-report").click(function() {
+        $(".submit-source-report").hide();
+        $(".main").css("opacity", 1);
+    })
 });
