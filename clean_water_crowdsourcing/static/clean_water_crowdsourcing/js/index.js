4635543c607ff8f86d55d247445c261cab8edf18
Sruti Guhathakurta <sruti@gatech.edu>
2016-12-03 15:41:46 -0500

    added submit purity report, edit profile, have to do ajax stuff

--- clean_water_crowdsourcing/static/clean_water_crowdsourcing/js/index.js
+++ clean_water_crowdsourcing/static/clean_water_crowdsourcing/js/index.js
@@ -11,4 +11,24 @@ $(document).ready(function() {
         $(".submit-source-report").hide();
         $(".main").css("opacity", 1);
     })
+    $("#submit-purity-nav").click(function() {
+            $(".submit-purity-report").show();
+            $(".main").css("opacity", 0.3);
+    })
+    $("#cancel-purity-report").click(function() {
+        $(".submit-purity-report").hide();
+        $(".main").css("opacity", 1);
+    })
+    $("#cancel-source-report").click(function() {
+            $(".submit-source-report").hide();
+            $(".main").css("opacity", 1);
+    })
+    $("#edit-profile-nav").click(function() {
+            $(".edit-profile").show();
+            $(".main").css("opacity", 0.3);
+    })
+    $("#cancel-edit-profile").click(function() {
+        $(".edit-profile").hide();
+        $(".main").css("opacity", 1);
+    })
 });
